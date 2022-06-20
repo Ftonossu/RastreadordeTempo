714f6ef3cd1599e42042b02beaa4adbb57d0edd0
Fabricio Tonossu <fabriciotonossu94@gmail.com>
2022-06-19 21:03:02 -0400

    Ajustes funcionalidades js

--- Projeto/js/formulário.js
+++ Projeto/js/formulário.js
@@ -6,29 +6,19 @@ const opcao = document.querySelectorAll('.opcao')
 
 const lista = document.querySelector('.lista')
 
-
 const titulo = document.querySelectorAll('.rastreador-titulo')
 
-console.log(rastreadorDiario)
+console.log(rastreadorDiario[0].value)
 
 botao.addEventListener("click", function(event){
     event.preventDefault();
-    console.log(horas.value)
-    console.log(lista.value)
     verifica();
-
 });
 
-function verifica(lista.value){
-    for(let i = 0; i < titulo.length; i++){
-        if(titulo[i].textContent = lista){
-            
-        }
+function verifica(){
+    console.log(lista.value);
+    console.log(titulo[0].textContent);
+    if(lista.value == titulo[0].textContent){
+        rastreadorDiario[0].textContent = 0 
     }
-
-}
-
-
-function addHoras(){
-    
 }
